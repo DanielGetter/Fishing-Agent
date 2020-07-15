ce84b4e759ffba3a6c4474ace2dc29b99dbc7763
Daniel G <dgetter_@berkeley.edu>
2020-07-15 13:10:03 -0700

    Tuned tf-agents agent with deeper Qnet and higher batch size

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="gym_fishing", 
-    version="0.0.2",
-    description="Provide gym environments for reinforcement learning",
-    packages=find_packages(),
-    python_requires='>=3.6',
-)
